d106bbb536c056b427aa0c93676a76a75edae07f
Aditya2623 <96367945+Aditya2623@users.noreply.github.com>
2023-08-15 17:58:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,29 +6,19 @@ import pandas as pd
 import openpyxl
 import hashlib
 from pymongo.mongo_client import MongoClient
-import mysql
-import pymysql
+
 app = Flask(__name__)
 
 # Set a secret key for the application
 app.secret_key = '123'
-#client = MongoClient("mongodb+srv://user_31:user31@cluster0.a5fjsp4.mongodb.net/?retryWrites=true&w=majority")
-#db = client['user_database']
+client = MongoClient("mongodb+srv://user_31:user31@cluster0.a5fjsp4.mongodb.net/?retryWrites=true&w=majority")
+db = client['user_database']
 
-#try:
-   # client.admin.command('ping')
-   # print("Pinged your deployment. You successfully connected to MongoDB!")
-#except Exception as e:
-    #print(e)
-# Establish a connection to the MySQL database
-db = pymysql.connect(
-    host='localhost',
-    user='root',
-    password='',
-    db='vocab1',
-    charset='utf8mb4',
-    cursorclass=pymysql.cursors.DictCursor
-)
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 # Set the upload folder path
 app.config['UPLOAD_FOLDER'] = 'uploads'
@@ -42,23 +32,6 @@ files = os.listdir(app.config['UPLOAD_FOLDER'])
 # Iterate over each file in the list and delete it
 for file in files:
     os.remove(os.path.join(app.config["UPLOAD_FOLDER"], file))
-
-def fetch_user_metadata(user_id):
-    try:
-        conn = mysql.connector.connect(**db)
-        cursor = conn.cursor(dictionary=True)
-        
-        query = "SELECT username, email FROM users WHERE id = %s"
-        cursor.execute(query, (user_id,))
-        user_metadata = cursor.fetchone()  # Assuming only one user is fetched
-        
-        return user_metadata
-    except mysql.connector.Error as err:
-        print(f"Error fetching user metadata: {err}")
-    finally:
-        if conn.is_connected():
-            cursor.close()
-            conn.close()
     
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -67,14 +40,13 @@ def signup():
         password = request.form['password']
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-        try:
-            with db.cursor() as cursor:
-                sql = "INSERT INTO users (username, password) VALUES (%s, %s)"
-                cursor.execute(sql, (username, hashed_password))
-                db.commit()
-            return redirect(url_for('login'))
-        except Exception as e:
-            return str(e)
+        user = {
+            'username': username,
+            'password': hashed_password
+        }
+
+        db.users.insert_one(user)
+        return redirect(url_for('login'))
 
     return render_template('signup.html')
 
@@ -85,18 +57,13 @@ def login():
         password = request.form['password']
         hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
-        try:
-            with db.cursor() as cursor:
-                sql = "SELECT * FROM users WHERE username=%s AND password=%s"
-                cursor.execute(sql, (username, hashed_password))
-                user = cursor.fetchone()
-                if user:
-                    session['username'] = username
-                    return redirect(url_for('profile'))
-                else:
-                    return "Invalid login credentials. Please try again."
-        except Exception as e:
-            return str(e)
+        user = db.users.find_one({'username': username, 'password': hashed_password})
+
+        if user:
+            session['username'] = username
+            return redirect(url_for('profile'))
+        else:
+            return "Invalid login credentials. Please try again."
 
     return render_template('signin.html')
 
@@ -104,13 +71,9 @@ def login():
 def profile():
     if 'username' in session:
         username = session['username']
-        
         return redirect(url_for('base'))
     else:
         return redirect(url_for('login'))
-        
-
-    
 
 
 
@@ -139,6 +102,8 @@ def sample(name):
     
     # Set the path where the WAV file will be saved
     wav_file_path = os.path.join(folder_path, f"{num}.wav")
+   
+    csv_file_path= os.path.join(folder_path, f"{num}.csv")
     
     # Save the WAV file to the specified path
     #wav_file.save(wav_file_path)
@@ -146,19 +111,15 @@ def sample(name):
     # Store the paths of the uploaded files in the session for later use
     session['excel_file_path'] = excel_file_path
     session['wav_file_path'] = wav_file_path
+    session['csv_file_path']=csv_file_path
+    session['folder_path']=folder_path
+    print(session['folder_path'])
+    
+    
 
     # Redirect the user to the "/render" route
     return redirect("/tool")
-
-@app.route('/get_user_metadata', methods=['GET'])
-def get_user_metadata():
-    # Replace this with actual database query to fetch user metadata
-    user_metadata = {
-        'username': 'example_user',
-        'email': 'user@example.com'
-        # ... other user metadata
-    }
-    return jsonify(user_metadata)
+    
 
 
 
@@ -173,12 +134,23 @@ def home():
     
     # Extract the columns from the DataFrame
     columns = df.columns[0:]
+    columns = df.columns.tolist()  # Convert Index object to a list
+    print("Before:", columns)
     
+# Insert an extra column name
+    extra_column_name = "Flag"
+    columns.insert(9, extra_column_name)
+    columns=['start time', 'end time', 'speaker id', 'act id', 'transcript', 'actual transcript','language', 'actual language', 'Flag']
+
+    print("After:", columns)
+    df[['start time', 'end time']] = df[['start time', 'end time']].round(1)
+    if 'id' in df.columns:
+       df=df.drop(['id'],axis=1)
     # Extract the data from the DataFrame
     data = df.iloc[:, 0:].values.tolist()
     
     # Create a set of unique speaker IDs from the "speaker_id" column in the Excel file
-    options = set(df["speaker_id"].unique())
+    options = set(df["speaker id"].unique())
     
     # Add additional options to the set
     options = options.union({'agent', 'speaker'})
@@ -188,15 +160,14 @@ def home():
     languages=["english","hindi"]
 
     ## Set the type of colummns and initial zoom
-    uneditable=[1,2,3,5,7,8]
+    uneditable=[1,2,3,5,7]
     editable=[6]
     zoom=float(df[columns[1]].iloc[-1])
     zoom=zoom/5
     if zoom>1000:
         zoom=1000
     # Render the 'index.html' template and pass the variables to it
-    return render_template('tool.html', columns=columns, data=data, wav_path=wav_path, options=options,languages=languages,editable=editable,uneditable=uneditable, zoom=zoom)
-
+    return render_template('toolold.html', columns=columns, data=data, wav_path=wav_path, options=options,languages=languages,editable=editable,uneditable=uneditable, zoom=zoom)
 
 
 @app.route("/")
@@ -206,26 +177,42 @@ def index():
 @app.route("/base")
 def base():
     ## get list of preloaded files
+    name=1
+    num = name
+    # Retrieve the uploaded files
+    folder_path = os.path.join(app.config['STATIC_FOLDER'], "pre-loaded", f"Call {num}")
+    session['folder_path']=folder_path
     file_names=os.listdir("static/pre-loaded")
+    
+    error_file=[]
     for i in range(len(file_names)):
         file_names[i]="Call "+str(i+1)
-    return render_template("home.html",filenames=file_names)
+        path=session['folder_path'][:-1]+str(i+1)
+        if  'reason.txt' in os.listdir(path) :
+           with open(path+'/reason.txt', 'r') as file:
+            if file.read() !='':
+                error_file.append("Call "+str(i+1))
+              
+            
+    return render_template("home.html",filenames=file_names,error_file=error_file)
 
 
 @app.route('/save', methods=['POST','GET'])
 def save():
     # Retrieve the JSON data from the request
+    print('******************************df.shape********************************************')
     data = request.get_json()
-
+    print('******************************df.shape********************************************')
     # Create an empty dictionary to store the data
     out_dict = {}
 
     # Read the Excel file using pandas
     df = pd.read_excel(session.get("excel_file_path"))
-
+    if 'id' in df.columns:
+        df=df.drop(['id'],axis=1)
     # Get the number of rows and columns in the DataFrame
     [rows, cols] = df.shape
-
+    
     # Iterate over each column in the DataFrame
     for i in range(0, cols):
         # Create a key in the dictionary for each column
@@ -239,14 +226,41 @@ def save():
 
     # Create a new DataFrame from the dictionary
     final_df = pd.DataFrame(out_dict)
-
+    
     # Overwrite the existing Excel file with the updated DataFrame
+
     final_df.to_excel(session.get('excel_file_path'), index=False)
 
+    final_df.to_csv(session.get('csv_file_path'), index=False)
+    
+   
+   
     # Return a success message
     return "Success"
 
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.get_json()
+    
+    selected_option = data['selectedOption']
+    
+    
+    # Process the selected option (you can replace this with your own logic)
+    if selected_option == 'cancel':
+        result = 'Action canceled.'
+    else:
+        result = selected_option
+   
+    print(result)
+    file_path=session['folder_path']+"/reason.txt"
+    with open(file_path, 'w') as file:
+            file.write(result)
+            print(f"Text saved to {file_path} successfully.")
+    return redirect(url_for('base'))
 
+    
+    
+    
 @app.route("/done", methods=['POST','GET'])
 def done():
     # Retrieve the path of the saved Excel file from the session
